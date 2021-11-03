d74e429b23d661fede9fecf854f5a00f5f10a9ff
sourabhkumar45 <sourabhkumar766709@gmail.com>
2021-11-03 23:33:41 +0530

    Ui is complete

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,18 @@
 
 let rows = 100;
 let cols = 26;
-
+// selection of HTML element
+/******************************************************/
 let addressColCont = document.querySelector(".address-col-container");
 // container name is colCont but it represent a row
 
 let addressRowCont = document.querySelector(".address-row-container");
 // container name is rowCont but it represent a col
 
+let cellCont = document.querySelector(".cells-container");
+let addressBar = document.querySelector(".address-bar");
+
+/******************************************************/
 //adding column container on the left side
 for (let i = 0; i < rows; i++) {
   let addressCol = document.createElement("div");
@@ -25,3 +30,26 @@ for (let i = 0; i < cols; i++) {
   addressRowCont.appendChild(addressRow);
 }
 
+// creating the grid
+for (let i = 0; i < rows; i++) {
+  let rowCont = document.createElement("div");
+  rowCont.setAttribute("class", "row-container");
+  for (let j = 0; j < cols; j++) {
+    let cell = document.createElement("div");
+    cell.setAttribute("class", "cell");
+    cell.setAttribute("contentEditable", true);
+    // adding event listenter for displaying cell address on address bar on click
+
+    addListenerForAddressBarDisplay(cell, i, j);
+    rowCont.appendChild(cell);
+  }
+  cellCont.appendChild(rowCont);
+}
+
+function addListenerForAddressBarDisplay(cell, i, j) {
+  cell.addEventListener("click", () => {
+    let rowId = i + 1;
+    let colId = String.fromCharCode(j + 65);
+    addressBar.value = `${colId}${rowId}`;
+  });
+}
